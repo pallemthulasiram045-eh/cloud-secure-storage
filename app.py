219c515e5644771c33108140b5dc8164481ccb63
Pallem Thulasi Ram <pallemthulasiram045@gmail.com>
2026-07-09 12:01:22 +0530

    Connect login page to Flask

--- app.py
+++ app.py
@@ -6,10 +6,18 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'cloud_secure_storage_secret_key'
 
 
+# Home Page
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
+# Login Page
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+# Run the Flask application
 if __name__ == '__main__':
     app.run(debug=True)
