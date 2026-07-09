db7d05e146067bebeb20912264a557c7f4156b98
Pallem Thulasi Ram <pallemthulasiram045@gmail.com>
2026-07-09 12:04:20 +0530

    Connect register page to Flask

--- app.py
+++ app.py
@@ -2,22 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Secret key for session management
+# Secret Key for Session Management
 app.config['SECRET_KEY'] = 'cloud_secure_storage_secret_key'
 
 
+# ==========================
 # Home Page
+# ==========================
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
+# ==========================
 # Login Page
+# ==========================
 @app.route('/login')
 def login():
     return render_template('login.html')
 
 
-# Run the Flask application
+# ==========================
+# Register Page
+# ==========================
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+# ==========================
+# Run Application
+# ==========================
 if __name__ == '__main__':
     app.run(debug=True)
